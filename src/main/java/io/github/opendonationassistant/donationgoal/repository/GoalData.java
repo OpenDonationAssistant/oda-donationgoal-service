9eae5f3e714aa12cbc2dedcb2d6e39001c24ccdd
stCarolas <stcarolas@gmail.com>
2025-06-26 02:58:03 +0300

    fix isDefault, fix reading goals without description

--- src/main/java/io/github/opendonationassistant/donationgoal/repository/GoalData.java
+++ src/main/java/io/github/opendonationassistant/donationgoal/repository/GoalData.java
@@ -5,6 +5,7 @@ import io.micronaut.data.annotation.Id;
 import io.micronaut.data.annotation.MappedEntity;
 import io.micronaut.data.annotation.MappedProperty;
 import io.micronaut.serde.annotation.Serdeable;
+import jakarta.annotation.Nullable;
 
 @Serdeable
 @MappedEntity("goal")
@@ -12,12 +13,12 @@ public record GoalData(
   @Id String id,
   String recipientId,
   String widgetId,
-  String briefDescription,
-  String fullDescription,
+  @Nullable String briefDescription,
+  @Nullable String fullDescription,
   Amount accumulatedAmount,
   Amount requiredAmount,
   Boolean enabled,
-  @MappedProperty("isdefault") Boolean isDefault
+  @MappedProperty("isDefault") Boolean isDefault
 ) {
   public GoalData withAccumulatedAmount(Amount amount) {
     return new GoalData(
